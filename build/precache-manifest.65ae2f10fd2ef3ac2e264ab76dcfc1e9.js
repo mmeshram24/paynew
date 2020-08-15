bfd0f16b1b2a1e7148781dafbdaa15a218b494ec
Mahavir <mmeshram24@gmail.com>
2020-08-15 13:05:02 +0530

    Add files via upload
    change ot odi

---
+++ build/precache-manifest.65ae2f10fd2ef3ac2e264ab76dcfc1e9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "11254b1a735ff2634f98",
+    "url": "/static/css/main.e211b439.chunk.css"
+  },
+  {
+    "revision": "11254b1a735ff2634f98",
+    "url": "/static/js/main.e934b534.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e4b938d03aedf0d4a46",
+    "url": "/static/css/2.a802106e.chunk.css"
+  },
+  {
+    "revision": "6e4b938d03aedf0d4a46",
+    "url": "/static/js/2.1440ae93.chunk.js"
+  },
+  {
+    "revision": "535309038a347d80db412e7911e1f006",
+    "url": "/static/media/topbar-image2.53530903.jpg"
+  },
+  {
+    "revision": "2bd412296331cbbba20cf2d208e4b426",
+    "url": "/static/media/topbar-image1.2bd41229.jpg"
+  },
+  {
+    "revision": "1e87a75387cd7673a2b1f6f61e2d9c17",
+    "url": "/static/media/topbar-image4.1e87a753.jpg"
+  },
+  {
+    "revision": "7ff2e995078ec6db99ef4cd6beb67a98",
+    "url": "/static/media/menu-image1.7ff2e995.jpg"
+  },
+  {
+    "revision": "38e60ed4eb3fcde81ecfe841678e088f",
+    "url": "/static/media/topbar-image3.38e60ed4.jpg"
+  },
+  {
+    "revision": "42ac80550015eecba1e8db7a5ea644ba",
+    "url": "/static/media/menu-image3.42ac8055.jpg"
+  },
+  {
+    "revision": "abe950513757efc57f6b40f82a27be8f",
+    "url": "/static/media/menu-image4.abe95051.jpg"
+  },
+  {
+    "revision": "dc299aa2a7afc2e2f808ca79dd461826",
+    "url": "/static/media/menu-image2.dc299aa2.jpg"
+  },
+  {
+    "revision": "557c102556347ca78d7fb3dda2ea1ac7",
+    "url": "/static/media/widget-blog.557c1025.jpg"
+  },
+  {
+    "revision": "15a3dc478f4c083c588f75ef987abfd6",
+    "url": "/static/media/login-bg.15a3dc47.jpg"
+  },
+  {
+    "revision": "e6a6e9604912b934bbee97a7971db77e",
+    "url": "/static/media/widget-weather.e6a6e960.jpg"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "73a932562a1e314703469d0a352fcda9",
+    "url": "/static/media/Simple-Line-Icons.73a93256.svg"
+  },
+  {
+    "revision": "8c3a3cbbba925bf57f0961d709028a69",
+    "url": "/index.html"
+  }
+];
